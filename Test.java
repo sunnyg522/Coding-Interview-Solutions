af1ca578ffb8035c0265e6d398ff1133e683c6ff
Dinesh Gunda <c17345@mbp-c17345.local>
2015-05-12 15:43:30 -0700

    test

--- Test.java
+++ Test.java
@@ -1,18 +1,7 @@
-public class StringReverse
+public class test
 {
-	public static void main(String[] args)
-	{
-		System.out.println(stringReverse("testing java project"));
-	}
-	public static String stringReverse(String s)
-	{
-		StringBuilder sb = new StringBuilder(s.length());
-		for(int i=s.length()-1; i>=0;i--)
-		{
-			 sb.append(s.charAt(i));
-		}
-
-		return sb.toString();
-
-	}
+  public static void main (String[] args)
+  {
+    System.out.println("Helow world");
+  }
 }
